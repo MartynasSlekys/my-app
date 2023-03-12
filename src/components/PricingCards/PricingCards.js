ca83eb516dda6bf6cd5c0fcaeaa62cbd10d5b8a3
MartynasSlekys <slekys.martynas@gmail.com>
2023-03-12 10:14:53 +0200

    created pricing cards .

---
+++ src/components/PricingCards/PricingCards.js
@@ -0,0 +1,45 @@
+import '../PricingCards/PricingCards.css';
+
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const PricingCards = () => {
+  return (
+    <div className='cards-wrapper'>
+        <div className='cards-content-wrapper'>
+            <div className='card'>
+                <h3>- Basic -</h3>
+                <span className='card-line'></span>
+                <p className='card-currency-title'>$ 100</p>
+                <p>- 3 days -</p>
+                <p>- 2 pages -</p>
+                <p>- Featured -</p>
+                <p>- Responsive Design -</p>
+                <Link to="/contact" className='btn'>ORDER NOW</Link>
+            </div>
+            <div className='card'>
+                <h3>- Premium -</h3>
+                <span className='card-line'></span>
+                <p className='card-currency-title'>$ 200</p>
+                <p>- 6 days -</p>
+                <p>- 4 pages -</p>
+                <p>- Featured -</p>
+                <p>- Responsive Design -</p>
+                <Link to="/contact" className='btn'>ORDER NOW</Link>
+            </div>
+            <div className='card'>
+                <h3>- Business -</h3>
+                <span className='card-line'></span>
+                <p className='card-currency-title'>$ 300</p>
+                <p>- 12 days -</p>
+                <p>- 6 pages -</p>
+                <p>- Featured -</p>
+                <p>- Responsive Design -</p>
+                <Link to="/contact" className='btn'>ORDER NOW</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PricingCards;
