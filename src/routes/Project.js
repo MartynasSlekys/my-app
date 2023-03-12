a51f59d2c2d52a09193245fb9f088abf8bfad641
MartynasSlekys <slekys.martynas@gmail.com>
2023-03-12 07:52:22 +0200

    page routing added nav bar, section, cards, footer

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import FooterSection from '../components/FooterSection/FooterSection';
+import NavigationBar from '../components/NavigationBar/NavigationBar';
+import PricingCards from '../components/PricingCards/PricingCards';
+import ProjectSection from '../components/ProjectSection/ProjectSection';
 
 const Project = () => {
   return (
-    <div>Project</div>
+    <div>
+      <NavigationBar />
+      <ProjectSection heading="Welcome to Projects." text="Lorem ipsum, or lipsum as it is sometimes known." />
+      <PricingCards />
+      <FooterSection />
+    </div>
   )
 }
 
-export default Project
+export default Project;
