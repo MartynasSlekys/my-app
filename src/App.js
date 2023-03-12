60cef6bdf2ad62e6ab194c28a735ddda9eea0f0d
MartynasSlekys <slekys.martynas@gmail.com>
2023-03-12 07:48:07 +0200

    page routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './index.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import Project from './routes/Project';
 
 function App() {
   return (
     <div>
-      <h1>This is my portfolio.</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
     </div>
   );
 }
