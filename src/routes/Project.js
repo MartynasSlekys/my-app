729e70d00bf9f9d022f98122fd100b6b5a82f236
MartynasSlekys <slekys.martynas@gmail.com>
2023-03-12 10:16:49 +0200

    at the project page added project cards.

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,12 +3,14 @@ import FooterSection from '../components/FooterSection/FooterSection';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
 import PricingCards from '../components/PricingCards/PricingCards';
 import ProjectSection from '../components/ProjectSection/ProjectSection';
+import ProjectWorkCardItem from '../components/ProjectWorkCardItem/ProjectWorkCardItem';
 
 const Project = () => {
   return (
     <div>
       <NavigationBar />
       <ProjectSection heading="Welcome to Projects." text="Lorem ipsum, or lipsum as it is sometimes known." />
+      <ProjectWorkCardItem />
       <PricingCards />
       <FooterSection />
     </div>
