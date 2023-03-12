42c69c8381c21c0ce0a323aa222cabf82dff8420
MartynasSlekys <slekys.martynas@gmail.com>
2023-03-12 10:16:03 +0200

    added about page component .

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutPageContent from '../components/AboutPageContent/AboutPageContent';
 import FooterSection from '../components/FooterSection/FooterSection';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
 import ProjectSection from '../components/ProjectSection/ProjectSection';
@@ -8,6 +9,7 @@ const About = () => {
     <div>
       <NavigationBar />
       <ProjectSection heading="About." text="Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print" />
+      <AboutPageContent />
       <FooterSection />
     </div>
   )
