bac2723c83f43aaeaee4281d6065a79f88c3d986
MartynasSlekys <slekys.martynas@gmail.com>
2023-03-12 10:16:30 +0200

    at home page added projects cards as component.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import FooterSection from '../components/FooterSection/FooterSection';
 import HeroSection from '../components/HeroSection/HeroSection';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
+import ProjectWorkCardItem from '../components/ProjectWorkCardItem/ProjectWorkCardItem';
 
 const Home = () => {
   return (
     <div>
         <NavigationBar />
         <HeroSection />
+        <ProjectWorkCardItem />
         <FooterSection />
     </div>
   )
