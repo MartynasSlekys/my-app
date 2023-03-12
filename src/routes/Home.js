b260231a31ad507f2cc4fcbd0c4cc568ac58bc92
MartynasSlekys <slekys.martynas@gmail.com>
2023-03-12 07:51:21 +0200

    fixed page routing, added nav bar, hero, footer.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import FooterSection from '../components/FooterSection/FooterSection';
+import HeroSection from '../components/HeroSection/HeroSection';
+import NavigationBar from '../components/NavigationBar/NavigationBar';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <NavigationBar />
+        <HeroSection />
+        <FooterSection />
+    </div>
   )
 }
 
-export default Home
+export default Home;
